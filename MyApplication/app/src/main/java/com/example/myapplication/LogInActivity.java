e66c4de29195e3f0d63654c6ca56625e13c524a3
yallyyally <jhshin0028@gmail.com>
2020-11-22 17:27:53 +0900

    modify Activity names
    MainActivity ->LogInActivity,
    MainActivity2->SignUnActivity,
    Main3Activity -> FavoriteInfoActivity

--- MyApplication/app/src/main/java/com/example/myapplication/LogInActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LogInActivity.java
@@ -6,24 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         findViewById(R.id.tv_sign_in).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
             }
         });
 
         findViewById(R.id.sign_up).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                startActivity(new Intent(LogInActivity.this, SignUpActivity.class));
             }
         });
     }
