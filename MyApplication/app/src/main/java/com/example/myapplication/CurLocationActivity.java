2a9425626baafe80b1b5c61a27fa2a7ced38dff4
yallyyally <jhshin0028@gmail.com>
2020-11-23 01:01:53 +0900

    Connect Map - Button(current location search)

--- MyApplication/app/src/main/java/com/example/myapplication/CurLocationActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/CurLocationActivity.java
@@ -2,60 +2,60 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
+public class CurLocationActivity extends AppCompatActivity {
 
-public class CurLocationActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+    public FragmentManager fragmentManager;
+    public EntertainOpenFragment fragmentEntertain;
+    public RestaurantOpenFragment fragmentRestaurant;
+    public CafeOpenFragment fragmentCafe;
 
-    private GoogleMap mMap;
-
+    public FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cur_location);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
         //use toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        //fragment handling
+        fragmentManager = getSupportFragmentManager();
 
-    @Override
-    public void onMapReady(final GoogleMap googleMap) {
+        fragmentCafe = new CafeOpenFragment();
+        fragmentEntertain = new EntertainOpenFragment();
+        fragmentRestaurant = new RestaurantOpenFragment();
 
-        mMap = googleMap;
-
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
-
-
-        // 기존에 사용하던 다음 2줄은 문제가 있습니다. 
-
-        // CameraUpdateFactory.zoomTo가 오동작하네요. 
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
-
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.FrameLayout_Map,fragmentRestaurant).commitAllowingStateLoss();
+        //레스토랑 버튼 잠시 바꿔준 상태...
 
     }
 
+
+    public void clickHandler(View view) {
+
+        transaction = fragmentManager.beginTransaction();
+
+        switch (view.getId())
+        {
+            case R.id.Button_Cafe:
+                transaction.replace(R.id.FrameLayout_Map,fragmentCafe).commitAllowingStateLoss();
+                break;
+            case R.id.Button_Entertain:
+                transaction.replace(R.id.FrameLayout_Map,fragmentEntertain).commitAllowingStateLoss();
+                break;
+            case R.id.Button_Restaurant:
+                transaction.replace(R.id.FrameLayout_Map,fragmentRestaurant).commitAllowingStateLoss();
+                break;
+        }
+    }
 }
