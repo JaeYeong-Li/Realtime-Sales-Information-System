6c8f552263211e3923e92cb0736988c3de15d137
yallyyally <jhshin0028@gmail.com>
2020-11-29 16:46:28 +0900

    Add Marker, distance calculation
    workspace.xml 충돌 처리, ResultViewApdater 충돌 땜빵

--- app/src/main/java/com/example/SWDesign_Team3_2020/ResultViewAdapter.java
+++ app/src/main/java/com/example/SWDesign_Team3_2020/ResultViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.SWDesign_Team3_2020;
 
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.content.Context;
 
@@ -9,5 +11,21 @@ public class ResultViewAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
+        return 1;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 }
