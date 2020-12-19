7c295ccecc015f6ef57e933aea2ee50675279a59
JaeYeong <42104898+JaeYeong-Li@users.noreply.github.com>
2020-12-19 22:58:23 +0900

    Update checkstore2.php

--- wamp64/www/checkstore2.php
+++ wamp64/www/checkstore2.php
@@ -7,20 +7,20 @@ include('dbcon.php');
 
 
 //POST 값을 읽어온다.
-$storeId=isset($_POST['storeId']) ? $_POST['storeId'] : '';
+$storeName=isset($_POST['storeName']) ? $_POST['storeName'] : '';
 $android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
 
 
-if ($storeId != "" ){ 
+if ($storeName != "" ){ 
 
-    $sql="select * from store where storeId='$storeId'";
+    $sql="select * from store where storeName='$storeName'";
     $stmt = $con->prepare($sql);
     $stmt->execute();
  
     if ($stmt->rowCount() == 0){
 
         echo "'";
-        echo $storeId;
+        echo $storeName;
         echo "'은 찾을 수 없습니다.";
     }
 	else{
@@ -33,13 +33,10 @@ if ($storeId != "" ){
 
             array_push($data, 
                 array('storeId'=>$row["storeId"],
-                'storeName'=>$row["storeName"],
-                'category'=>$row["category"],
-                'lat'=>$row["lat"],
-                'lang'=>$row["lang"],
                 'address'=>$row["address"],
                 'menu'=>$row["menu"],
                 'openTime'=>$row["openTime"],
+                'openDate'=>$row["openDate"],
                 'photoUrl1'=>$row["photoUrl1"]
         ));
         }
